58d95ea1406ed40c090d5b5c7803f4b585511156
xpr123 <3424286550@qq.com>
2025-03-12 21:10:58 +0800

    扫雷游戏

--- 游戏/sao_lei/sao_lei/game.h
+++ 游戏/sao_lei/sao_lei/game.h
@@ -1,19 +1,27 @@
 #pragma once
+
 #include<stdio.h>
 #include<stdlib.h>
+#include<time.h>
 
 #define ROW 9
 #define COL 9
 
-#define ROWS ROW + 2 
-#define COLS COL + 2
+#define ROWS ROW+2
+#define COLS COL+2
 
+//雷的数量
 #define EASY_COUNT 10
+
+//实现扫雷游戏中的函数声明
 //初始化棋盘
 void InitBoard(char board[ROWS][COLS], int rows, int cols, char set);
 
 //打印棋盘
-void DisplayBoard(char board[ROWS][COLS], int row, int col);
+void DisPlayBoard(char board[ROWS][COLS], int row, int col);
 
 //布置雷
-void SetMine(char mine[ROWS][COLS], int row, int col);
+void SetMine(char board[ROWS][COLS], int row, int col);
+
+//排查雷
+void FindMine(char board[ROWS][COLS], char show[ROWS][COLS], int row, int col);
