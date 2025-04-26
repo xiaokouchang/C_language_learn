d96646cdc2129c60a8d4906e8e26842a415f3649
xpr123 <3424286550@qq.com>
2025-04-26 18:47:21 +0800

    修改通讯录

--- 游戏/tong_xun_lu/tong_xun_lu/contact.h
+++ 游戏/tong_xun_lu/tong_xun_lu/contact.h
@@ -2,12 +2,15 @@
 #include <stdio.h>
 #include <string.h>
 #include <assert.h>
+#include <stdlib.h>
 
 #define MAX 100
 #define NAME_MAX 20
 #define SEX_MAX 5
 #define ADDR_MAX 30
 #define TELE_MAX 12
+#define DEFAULT_SZ 3   //默认大小
+#define INC_SZ 2   //增加大小
 //人的信息
 typedef struct PeoInfo
 {
@@ -20,11 +23,20 @@ typedef struct PeoInfo
 
 
 //通讯录
+//typedef struct contact
+//{
+//	PeoInfo data[MAX];//存放当前人的信息
+//	int sz;//当前已经放的信息的总人数
+//}contact;
+
+//动态版本
 typedef struct contact
 {
-	PeoInfo data[MAX];//存放当前人的信息
+	PeoInfo* data;//指向存放当前人的信息的空间
 	int sz;//当前已经放的信息的总人数
+	int capacity;//当前通讯录的最大容量
 }contact;
+
 
 //通讯录初始化
 void Initcontact(contact* pc);
@@ -49,3 +61,9 @@ void Modifycontact(contact* pc);
 
 //排序
 void Sortcontact(contact* pc);
+
+//检查容量
+void check_capacity(contact* pc);
+
+//销毁通讯录
+void DestoryContact(contact* pc);
