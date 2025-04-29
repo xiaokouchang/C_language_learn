9a8b6ee223ca016833367617201443b06c194cf0
xpr123 <3424286550@qq.com>
2025-04-29 11:11:39 +0800

    文件缓冲区

--- wen_jian/wen_jian/test.c
+++ wen_jian/wen_jian/test.c
@@ -116,26 +116,26 @@
 //}
 
 
-#include <stdio.h>
-#include <string.h>
-int main()
-{
-	FILE* pf = fopen("test.txt", "r");//根目录,test.c文件的位置
-	if (pf == NULL)
-	{
-		perror("fopen");
-		return 1;
-	}
-	char arr[20]= { 0 };
-	fgets(arr, 19, pf);
-	printf("%s", arr);
-	fgets(arr, 19, pf);
-	printf("%s", arr);
-	//关闭文件
-	fclose(pf);
-	pf = NULL;
-	return 0;
-}
+//#include <stdio.h>
+//#include <string.h>
+//int main()
+//{
+//	FILE* pf = fopen("test.txt", "r");//根目录,test.c文件的位置
+//	if (pf == NULL)
+//	{
+//		perror("fopen");
+//		return 1;
+//	}
+//	char arr[20]= { 0 };
+//	fgets(arr, 19, pf);
+//	printf("%s", arr);
+//	fgets(arr, 19, pf);
+//	printf("%s", arr);
+//	//关闭文件
+//	fclose(pf);
+//	pf = NULL;
+//	return 0;
+//}
 
 
 //fputs
@@ -424,8 +424,9 @@ int main()
 //	int a = 10000;
 //	FILE* pf = fopen("test.txt", "wb");
 //	fwrite(&a, 4, 1, pf);//二进制的形式写到文件中
-//	//10       27       00       00
+//	//00000000 00000000 00100111 00010000
 //	//00010000 00010111 00000000 00000000
+//	//10       27       00       00
 //	fclose(pf);
 //	pf = NULL;
 //	return 0;
@@ -444,3 +445,105 @@ int main()
 //首先文件读取结束了,结束后想知道文件读取的原因
 //feof   --- 返回真,就说明是正常读取遇到了结束标志而结束
 //ferror --- 返回真说明是文件读取过程中出错了,结束了
+//#include <stdio.h>
+//#include <stdlib.h>
+//int main(void)
+//{
+//	int c; //注意:int,非char,要求处理EOF
+//	FILE* fp = fopen("test.txt", "r");
+//	if (!fp) 
+//	{
+//		perror("File opening failed");
+//		return EXIT_FAILURE;
+//	}
+//	//fgetc当读取失败的时候或者遇到文件结束的时候,都会返回EOF
+//	while ((c = fgetc(fp)) != EOF) //标准C I/O读取文件循环
+//	{
+//		putchar(c);
+//	}
+//	//判断是什么原因结束的
+//	if (ferror(fp))
+//	{
+//		puts("I/O error when reading");
+//	}
+//	else if (feof(fp))
+//	{
+//		puts("End of file reached successfully");
+//	}
+//	fclose(fp);
+//	fp = NULL;
+//	return 0;
+//}
+
+
+//#include <stdio.h>
+//enum
+//{
+//	SIZE = 5 
+//};
+//int main(void)
+//{
+//	double a[SIZE] = { 1.,2.,3.,4.,5. };
+//	FILE* fp = fopen("test.bin", "wb"); //必须用二进制模式
+//	fwrite(a, sizeof * a, SIZE, fp); //写double的数组
+//	fclose(fp);
+//	double b[SIZE];
+//	fp = fopen("test.bin", "rb");
+//	size_t ret_code = fread(b, sizeof * b, SIZE, fp); //读double的数组
+//	if (ret_code == SIZE) 
+//	{
+//		puts("Array read successfully, contents: ");
+//		for (int n = 0; n < SIZE; ++n)
+//		{
+//			printf("%f ", b[n]);
+//		}
+//		putchar('\n');
+//	}
+//	else 
+//	{ // error handling
+//		if (feof(fp))
+//		{
+//			printf("Error reading test.bin: unexpected end of file\n");
+//		}
+//		else if (ferror(fp)) 
+//		{
+//			perror("Error reading test.bin");
+//		}
+//	}
+//	fclose(fp);
+//	fp = NULL;
+//	return 0;
+//}
+
+
+//文件缓冲区
+//ANSIC标准采用"缓冲文件系统"处理的数据文件的,
+//所谓缓冲文件系统是指系统自动地在内存中为程序中每一个正在使用的文件开辟一块"文件缓冲区"
+//从内存向磁盘输出数据会先送到内存中的缓冲区,装满缓冲区后才一起送到磁盘上
+//如果从磁盘向计算机读入数据,则从磁盘文件中读取数据输入到内存缓冲区(充满缓冲区),
+//然后再从缓冲区逐个地将数据送到程序数据区(程序变量等)
+//缓冲区的大小根据C编译系统决定的
+
+//因为有缓冲区的存在,C语言在操作文件的时候,需要做刷新缓冲区或者在文件操作结束的时候关闭文件
+//如果不做,可能导致读写文件的问题
+
+//缓冲区放满或者刷新缓冲区
+//#include <stdio.h>
+//#include <windows.h>
+////VS2022 WIN11环境测试
+//int main()
+//{
+//	FILE* pf = fopen("test.txt", "w");
+//	fputs("abcdef", pf);//先将代码放在输出缓冲区
+//	printf("睡眠10秒-已经写数据了,打开test.txt文件,发现文件没有内容\n");
+//	Sleep(10000);//10000毫秒
+//	printf("刷新缓冲区\n");
+//	fflush(pf);//刷新缓冲区时,才将输出缓冲区的数据写到文件(磁盘)
+//	//注:fflush在高版本的VS上不能使用了
+//	printf("再睡眠10秒-此时,再次打开test.txt文件,文件有内容了\n");
+//	Sleep(10000);
+//	fclose(pf);
+//	//注:fclose在关闭文件的时候,也会刷新缓冲区
+//	pf = NULL;
+//	return 0;
+//}
